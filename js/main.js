b74dbb242dd6a204e077d3de6e7f290f9f359dd3
LiXiang <toby91511@gmail.com>
2023-12-24 03:13:08 +0800

    新增漢堡動畫

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 let nav = document.querySelector(".main-nav");
 let hb = document.querySelector(".hb");
 
-if (window.innerWidth < 993) {
-    $('.click').click(function () {
-        nav.style.display = "none";
-    });
-    $('.hb').click(function () {
-        nav.style.display = "block";
-    });
+// if (window.innerWidth < 993) {
+//     $('.click').click(function () {
+//         nav.style.display = "none";
+//     });
+//     $('.hb').click(function () {
+//         nav.style.display = "block";
+//     });
     
-}
+// }
 function Confetti() {
     var confettiSettings = { target: 'my-canvas' };
     var confetti = new ConfettiGenerator(confettiSettings);
@@ -18,3 +18,11 @@ function Confetti() {
         confetti.clear();
     }, 3000)
 }
+  // nav選單動畫
+  let menuBtn = document.querySelectorAll(".icon");
+  for (var i = 0; i < menuBtn.length; i++) {
+      menuBtn[i].addEventListener("click", (e) => {
+          console.log(e.target);
+          e.target.classList.toggle("active");
+      });
+  }
